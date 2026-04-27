2cdba7dd7970bdca2b8138053f4d6761d11df9f2
vitoriadasilvasousavivi-cmd <vitoriadasilvasousavivi@gmail.com>
2026-04-27 11:35:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import createItem from "./service/item.js";
+
 const cart = [];
+
+console.log("Welcome to the your Shopee Cart!");
+
+const item1 = createItem("Colar", 10.00,1);
+const item2 = createItem("Pulsera", 12.00,2);
+
+
+
+
