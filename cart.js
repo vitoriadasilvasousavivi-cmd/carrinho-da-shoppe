6bbb62c84eb7366e9ddf043730f7556e59575eb0
vitoriadasilvasousavivi-cmd <vitoriadasilvasousavivi@gmail.com>
2026-04-27 11:34:53 -0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,12 +1,24 @@
-// quias ações meu carrinhopode fazer 
+// quias ações meu carrinho pode fazer 
 
+//caso de uso
+//adicionar itens no carrinho
 adicionar item no carrinho
-
+async function addItem(userCart) {
+    
+}
 
 
 deletar item do carrinho
-
-
-
+async function deletarItem(userCart, name){
+    
+}
 
 remove item 
+async function removeItem(userCart, index){
+    
+}
+
+calcular total
+async function calculateTotal(userCart){
+    
+}
