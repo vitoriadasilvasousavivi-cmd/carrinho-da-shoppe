fdde12d61caf13ef9b85742448cbecac8616e73a
vitoriadasilvasousavivi-cmd <vitoriadasilvasousavivi@gmail.com>
2026-04-30 12:55:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import * as cartService
-
+import * as cartService from ./services/cart.js*
 import createItem from "./service/item.js";
 
 const cart = [];
@@ -9,6 +8,17 @@ console.log("Welcome to the your Shopee Cart!");
 const item1 = await createItem("Colar", 10.00,1);
 const item2 = await createItem("Pulsera", 12.00,2);
 
-console.log(item2.subtotal());
+await cartService.removeItem(myCart, item1);
+await cartService.removeItem(myCart, item2);
+
+await cartService.removeItem(myCart, item2);
+await cartService.removeItem(myCart, item2);
+await cartService.removeItem(myCart, item2);
+
+await cartService.display(myCart);
+// deleei 2 itens do carrinho
+// await cartService.addItem(myCart, item2.name);
+// await cartService.addItem(myCart, item2.name);
+await cartSevice.calculeTotal(myCart);
 
 
